819ec109555641c964996a0e14ea2351d7c038a4
Juliette Rapala <jrapala@gmail.com>
2020-11-12 18:20:26 -0600

    feat: add iTunes API call and render content

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var results = [Result]()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }
+        .onAppear(perform: loadData)
+    }
+    
+    func loadData() {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                    // we have good data – go back to the main thread
+                    DispatchQueue.main.async {
+                        // update our UI
+                        self.results = decodedResponse.results
+                    }
+
+                    // everything is good, so we can exit
+                    return
+                }
+            }
+
+            // if we're still here it means there was a problem
+            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+        }.resume()
+        
     }
 }
 
@@ -30,6 +66,16 @@ class User: ObservableObject, Codable {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(name, forKey: .name)
     }
+}
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
 }
 
 struct ContentView_Previews: PreviewProvider {
