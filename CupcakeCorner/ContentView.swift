dda6af4115a6dfa5f9039ff837465b2da2dbef5b
Juliette Rapala <jrapala@gmail.com>
2020-11-12 18:16:53 -0600

    feat: create User class

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -14,6 +14,24 @@ struct ContentView: View {
     }
 }
 
+class User: ObservableObject, Codable {
+    @Published var name = "Paul Hudson"
+    
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
