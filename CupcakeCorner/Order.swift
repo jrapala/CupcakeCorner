46e773ffd29aeefae5230f62b5b8b2b341f42d7a
Juliette Rapala <jrapala@gmail.com>
2020-11-13 19:16:04 -0600

    feat: add order class

---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,19 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Juliette Rapala on 11/13/20.
+//
+
+import Foundation
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    
+    @Published var specialRequestEnabled = false
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+}
