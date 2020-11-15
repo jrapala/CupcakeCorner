0af2f6851d2665bb74e5ff53f0e9e3b56451ba90
Juliette Rapala <jrapala@gmail.com>
2020-11-15 17:57:28 -0600

    feat: add Codable conformance

--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -7,7 +7,11 @@
 
 import Foundation
 
-class Order: ObservableObject {
+// Swift doesn't understand how to encode and decode published properties
+// Therefore, we must add Codable conformance by hand by telling Swift how to
+// ebcide abd decode JSON to Swift data (and back)
+// Follow steps 1-4
+class Order: ObservableObject, Codable {
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
     
     @Published var type = 0
@@ -28,6 +32,43 @@ class Order: ObservableObject {
     @Published var streetAddress = ""
     @Published var city = ""
     @Published var zip = ""
+
+    // 1. Add enum for Published properties
+    enum CodingKeys: CodingKey  {
+        case type, quantity, extraFrosting, addSprinkles, name, streetAddress, city, zip
+    }
+    
+    
+    // 2. Write an encode method
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(type, forKey: .type)
+        try container.encode(quantity, forKey: .quantity)
+        try container.encode(extraFrosting, forKey: .extraFrosting)
+        try container.encode(addSprinkles, forKey: .addSprinkles)
+        try container.encode(name, forKey: .name)
+        try container.encode(streetAddress, forKey: .streetAddress)
+        try container.encode(city, forKey: .city)
+        try container.encode(zip, forKey: .zip)
+    }
+    
+    // 3. Write an initializer that decodes
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        type = try container.decode(Int.self, forKey: .type)
+        quantity = try container.decode(Int.self, forKey: .quantity)
+        extraFrosting = try container.decode(Bool.self, forKey: .extraFrosting)
+        addSprinkles = try container.decode(Bool.self, forKey: .addSprinkles)
+        name = try container.decode(String.self, forKey: .name)
+        streetAddress = try container.decode(String.self, forKey: .streetAddress)
+        city = try container.decode(String.self, forKey: .city)
+        zip = try container.decode(String.self, forKey: .zip)
+    }
+    
+    // 4. Add an empty initializer that can create an order without any data whatsoever
+    init() {}
     
     var hasValidAddress: Bool {
         if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
