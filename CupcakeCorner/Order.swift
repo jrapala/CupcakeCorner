2f2e00271231b646bc04273d0f0aac3a35b4a597
Juliette Rapala <jrapala@gmail.com>
2020-11-16 11:40:28 -0600

    fix: improve address validation

--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -73,9 +73,12 @@ class Order: ObservableObject, Codable {
     init() {}
     
     var hasValidAddress: Bool {
-        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
-            return false
-        }
+        if name.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+            || streetAddress.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+            || city.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+            || zip.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+                return false
+            }
         
         return true
     }
