7f50439105d7e06c542c674cf35e5299e1cf55ac
Juliette Rapala <jrapala@gmail.com>
2020-11-15 18:48:43 -0600

    feat: POST data to API

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CheckoutView: View {
     @ObservedObject var order: Order
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
     
     var body: some View {
         GeometryReader { geo in
@@ -23,13 +25,49 @@ struct CheckoutView: View {
                         .font(.title)
                     
                     Button("Place Order") {
-                        
+                        self.placeOrder()
                     }
                     .padding()
                 }
             }
         }
         .navigationBarTitle("Check Out", displayMode: .inline)
+        .alert(isPresented: $showingConfirmation) {
+            Alert(title: Text("Thank you!"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+        }
+    }
+    
+    func placeOrder() {
+    // 1. Convert order to JSON
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+    // 2. Prepare a URLRequest
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encoded
+        
+        
+    // 3. Run the request and process response
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print("No data in response: \(error?.localizedDescription ?? "Unknown error").")
+                return
+            }
+
+            if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+                self.confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+                self.showingConfirmation = true
+            } else {
+                print("Invalid response from server")
+            }
+            
+        }
+        .resume()
     }
 }
 
